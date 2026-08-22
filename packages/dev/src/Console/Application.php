5f271853deef602b2adc461e8c152b5388baa89c
Jonathan Gonçalves <jonathan@themattos.dev>
2026-08-22 03:43:29 +0200

    chore: bump version to 0.7.0

--- packages/dev/src/Console/Application.php
+++ packages/dev/src/Console/Application.php
@@ -10,7 +10,7 @@ use TheMattos\Leakless\Dev\Console\Commands\AnalyzeCommand;
 
 final class Application extends BaseApplication
 {
-    public const VERSION = '0.6.0';
+    public const VERSION = '0.7.0';
 
     public function __construct()
     {
